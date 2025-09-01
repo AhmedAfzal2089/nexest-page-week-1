aeac172023a9ee7292432f600f0b8fc87c261f69
AhmedAfzal2089 <ameafzal@gmail.com>
2025-09-01 17:19:26 +0500

    add ourClients Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import OurClientSection from "./components/OurClientSection";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <OurClientSection />
     </>
   );
 };
---
+++ src/components/OurClientSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import FirstLogo from "../../public/client svgs/firstLogo";
+import SecondLogo from "../../public/client svgs/secondLogo";
+import ThirdLogo from "../../public/client svgs/thirdLogo";
+import FourthLogo from "../../public/client svgs/fourthLogo";
+import FifthLogo from "../../public/client svgs/fifthLogo";
+import SixthLogo from "../../public/client svgs/sixthLogo";
+import SeventhLogo from "../../public/client svgs/seventhLogo";
+
+const OurClientSection = () => {
+  return (
+    <div className="flex flex-col items-center gap-7  px-5">
+      <span className="font-bold text-4xl text-gray-600 mt-8">Our Clients</span>
+      <span className="text-gray-500 text-center max-w-md ">
+        We have been working with some Fortune 500+ clients
+      </span>
+      <div className="grid grid-cols-3 sm:grid-cols-3 gap-6 lg:flex lg:gap-35 lg:flex-wrap lg:justify-center">
+        <FirstLogo />
+        <SecondLogo />
+        <ThirdLogo />
+        <FourthLogo />
+        <FifthLogo />
+        <SixthLogo />
+        <SeventhLogo />
+      </div>
+    </div>
+  );
+};
+
+export default OurClientSection;
